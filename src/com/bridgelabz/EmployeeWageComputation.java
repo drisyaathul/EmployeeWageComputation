2e63cd5afb29a99dd6d3b779d5e8aa7b86bb2168
drisyaathul <drisyak809@gmail.com>
2023-07-20 00:06:59 +0800

    Added: Employee Attendence Using RANDOM

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("*** Welcome To Employee Wage Computation Program ***");
+        //UC1:Check Employee is Present or Absent
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else
+            System.out.println("Employee is Absent");
     }
 }
