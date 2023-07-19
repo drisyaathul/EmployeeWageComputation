aa8f33767112299999af46a58588be14c123079b
drisyaathul <drisyak809@gmail.com>
2023-07-20 02:02:04 +0800

    Added: Employee Wages till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -9,30 +9,37 @@ public class EmployeeWageComputation {
         int wagePerHour = 20;
         int workingHour = 0;
         int workingDays = 20;
-        
-        Random random = new Random();
-        int attendance = random.nextInt(3);
+        int maxWorkingHours = 100;
+        int monthlyWage = 0;
+        int dailyWage = 0;
+        int day = 1;
 
-        switch (attendance) {
-            case 0:
-                System.out.println("Employee is Absent");
-                workingHour = 0;
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                workingHour = 8;
-                break;
-            case 2:
-                System.out.println("Employee is Part-Time");
-                workingHour = 4;
-                break;
-            default:
-                System.out.println("INVALID");
+        while (day <= workingDays && workingHour <= maxWorkingHours) {
+            Random random = new Random();
+            int attendance = random.nextInt(3);
+            System.out.println("Day => "+day);
+            switch (attendance) {
+                case 0:
+                    System.out.println("Employee is Absent");
+                    workingHour = 0;
+                    break;
+                case 1:
+                    System.out.println("Employee is Present");
+                    workingHour = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is Part-Time");
+                    workingHour = 4;
+                    break;
+            }
+            day++;
+            dailyWage = workingHour * wagePerHour;
+            monthlyWage += dailyWage;
+            //maxWorkingHours += workingHour;
+            System.out.println("Employee Daily Wage : " +dailyWage);
+           //System.out.println("Employee Total Working Hours : "+maxWorkingHours);
         }
-        int dailyWage = workingHour * wagePerHour;
-        int monthlyWage = workingDays * dailyWage;
-        System.out.println("Employee Daily Wage : "+dailyWage);
-        System.out.println("Employee Monthly Wage : "+monthlyWage);
-
+        System.out.println("**************************************************");
+        System.out.println("Employee Monthly Wage : " +monthlyWage+" $");
     }
 }
